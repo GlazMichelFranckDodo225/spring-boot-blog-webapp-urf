dab6b64d299d9472ce91e34d99d8a725845c43fc
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-08 17:04:02 +0200

    Blog Search Feature Backend Implementation

--- src/main/java/com/dgmf/controller/BlogController.java
+++ src/main/java/com/dgmf/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,5 +32,15 @@ public class BlogController {
         model.addAttribute("post", post);
 
         return "blog/blog_post";
+    }
+
+    // Handler Method for Bog Post Search Request
+    // http://localhost:8080/page/search?query=java
+    @GetMapping("/page/search")
+    public String searchPosts(@RequestParam("query") String query, Model model) {
+        List<PostDto> postsResponse = postService.searchPosts(query);
+        model.addAttribute("postsResponse", postsResponse);
+
+        return "blog/view_posts";
     }
 }
