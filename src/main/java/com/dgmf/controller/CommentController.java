666130275401bf9940ce959eb3c42f48873fdf5e
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-25 04:50:59 +0200

    Check If There Is Any Error While Creating a Comment

--- src/main/java/com/dgmf/controller/CommentController.java
+++ src/main/java/com/dgmf/controller/CommentController.java
@@ -1,11 +1,14 @@
 package com.dgmf.controller;
 
 import com.dgmf.dto.CommentDto;
+import com.dgmf.dto.PostDto;
 import com.dgmf.service.CommentService;
+import com.dgmf.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +17,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+    private final PostService postService;
 
     // Handler Method to Create Form Submit Request
     @PostMapping("/{postUrl}/comments")
     public String createComment(
             @Valid @PathVariable("postUrl") String postUrl,
             @ModelAttribute("comment") CommentDto commentDto,
-            Model model
+            Model model,
+            BindingResult result
     ) {
+        PostDto postDto = postService.findPostByUrl(postUrl);
+
+        // Check If There Is Any Error While Creating a Comment
+        if (result.hasErrors()) {
+            model.addAttribute("post", postDto);
+            model.addAttribute("comment", commentDto);
+
+            return "blog/blog_post";
+        }
+
         commentService.createComment(postUrl, commentDto);
 
         return "redirect:/post/" + postUrl;
