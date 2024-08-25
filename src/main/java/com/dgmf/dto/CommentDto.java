95bee2115a9becf9065c197e9bc09efc29892869
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-25 04:36:14 +0200

    Add Validations to the CommentDto Class

--- src/main/java/com/dgmf/dto/CommentDto.java
+++ src/main/java/com/dgmf/dto/CommentDto.java
@@ -1,5 +1,7 @@
 package com.dgmf.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,11 +9,16 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+// Acts as a Model Object into Create Comment Form
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class CommentDto {
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty(message = "Email Should Not Be Empty or Null")
+    @Email
     private String email;
+    @NotEmpty(message = "Message Body Should Not Be Empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
