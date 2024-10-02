6013e7de76cb03ecac3d3fbba8983e542b80f63b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-10-02 07:36:48 +0200

    Create Handler Method to Save User Registered Data

--- src/main/java/com/dgmf/controller/AuthController.java
+++ src/main/java/com/dgmf/controller/AuthController.java
@@ -1,12 +1,19 @@
 package com.dgmf.controller;
 
 import com.dgmf.dto.RegistrationDto;
+import com.dgmf.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserService userService;
+
     // Handler Method for User Registration Request
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
@@ -15,5 +22,13 @@ public class AuthController {
         model.addAttribute("user", user);
 
         return "register";
+    }
+
+    // Handler Method for User Registration Form Submit Request
+    @PostMapping("/register/save")
+    public String register(@ModelAttribute("user") RegistrationDto user) {
+        userService.saveUser(user);
+
+        return "redirect:/register?success";
     }
 }
