d8f840b6a00d6ab8ac4710d59da534c4879f8c87
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-10-05 07:20:47 +0200

    Adding Validation to RegistrationDto Class

--- src/main/java/com/dgmf/dto/RegistrationDto.java
+++ src/main/java/com/dgmf/dto/RegistrationDto.java
@@ -1,5 +1,7 @@
 package com.dgmf.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,8 +10,13 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class RegistrationDto {
     private Long id;
+    @NotEmpty(message = "First Name Should Not Be Empty or Null")
     private String firstName;
+    @NotEmpty(message = "Last Name Should Not Be Empty or Null")
     private String lastName;
+    @NotEmpty(message = "Email Should Not Be Empty or Null")
+    @Email
     private String email;
+    @NotEmpty(message = "Password Should Not Be Empty")
     private String password;
 }
