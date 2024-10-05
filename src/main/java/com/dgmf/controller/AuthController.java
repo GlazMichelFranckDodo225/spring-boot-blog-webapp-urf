af4c07c4d8e7f44b861866c1b8080d4f9cdbf830
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-10-05 07:29:47 +0200

    Form Errors Checking at Handler Method Level

--- src/main/java/com/dgmf/controller/AuthController.java
+++ src/main/java/com/dgmf/controller/AuthController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,18 @@ public class AuthController {
 
     // Handler Method for User Registration Form Submit Request
     @PostMapping("/register/save")
-    public String register(@Valid @ModelAttribute("user") RegistrationDto user) {
+    public String register(
+            @Valid @ModelAttribute("user") RegistrationDto user,
+            BindingResult result,
+            Model model
+            )
+    {
+        if(result.hasErrors()) {
+            model.addAttribute("user", user);
+
+            return "register";
+        }
+        
         userService.saveUser(user);
 
         return "redirect:/register?success";
